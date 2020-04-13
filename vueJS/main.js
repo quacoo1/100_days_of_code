b7f41b6b378b92c402f8060c697159ba4eec6193
Duality <quacoo.boat@gmail.com>
2020-04-13 18:40:53 +0000

    lesson2

---
+++ vueJS/main.js
@@ -0,0 +1,8 @@
+const app = new Vue ({
+    el: "#app",
+    data: {
+        image: './light_blue.png',
+        product: "LightSaber",
+        description: "May the force help you slice every Sith Head"
+    }
+})
