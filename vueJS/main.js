025d8b531cf711c76aa799f1d8a58773d58e3618
Duality <quacoo.boat@gmail.com>
2020-04-13 23:38:56 +0000

    lesson5

--- vueJS/main.js
+++ vueJS/main.js
@@ -1,10 +1,42 @@
 const app = new Vue ({
     el: "#app",
+
     data: {
+
         image: './light_blue.png',
         product: "LightSaber",
         description: "May the force help you slice every Sith Head",
         inStock: true,
         details:['10GW ion energy cell','4D emitter matrix','f/2 Flux aperture '],
-    }
+        cart: 0,
+
+        variants: [
+            {
+                variantId :1123,
+                variantColor: 'Blue',
+                variantImage: './light_blue.png'
+            },
+            {
+                variantId: 1124,
+                variantColor: 'Red',
+                variantImage: './light_red.png'
+
+            },
+        ],
+
+    },
+
+    methods: {
+
+        addToCart: function(){
+            this.cart++;
+        },
+
+        updateImage: function(variantImage){
+            this.image = variantImage;
+        },
+
+
+
+    },
 })
