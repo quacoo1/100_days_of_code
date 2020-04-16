6727e3e159eb89acb1bcc4bded2a6964baa83517
Duality <quacoo.boat@gmail.com>
2020-04-16 12:49:11 +0000

    lesson6b

--- vueJS/main.js
+++ vueJS/main.js
@@ -2,26 +2,25 @@ const app = new Vue ({
     el: "#app",
 
     data: {
-
-        image: './light_blue.png',
         product: "LightSaber",
         description: "May the force help you slice every Sith Head",
-        inStock: false,
         details:['10GW ion energy cell','4D emitter matrix','f/2 Flux aperture '],
         cart: 0,
         brand: 'Republic',
+        index: 0,
 
         variants: [
             {
                 variantId :1123,
                 variantColor: 'Blue',
-                variantImage: './light_blue.png'
+                variantImage: './light_blue.png',
+                variantStock: 40,
             },
             {
                 variantId: 1124,
                 variantColor: 'Red',
-                variantImage: './light_red.png'
-
+                variantImage: './light_red.png',
+                variantStock: 0,
             },
         ],
 
@@ -33,11 +32,9 @@ const app = new Vue ({
             this.cart++;
         },
 
-        updateImage(variantImage){
-            this.image = variantImage;
+        updateImage(index){
+            this.index = index;
         },
-
-
 
     },
 
@@ -45,6 +42,13 @@ const app = new Vue ({
 
         title(){
             return `${this.brand} ${this.product}`
+        },
+        image(){
+            return this.variants[this.index].variantImage;
+        },
+
+        inStock(){
+            return this.variants[this.index].variantStock;
         }
 
     },
