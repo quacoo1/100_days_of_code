fac7f9f83d4b7a1217d3d465a486495e41cea9db
Duality <quacoo.boat@gmail.com>
2020-04-17 22:55:38 +0000

    lesson7 the Components a first look

--- vueJS/description.md
+++ vueJS/description.md
@@ -5,3 +5,10 @@
 # questions:
 # 1) by don't we just use expressions in html attributes instead of attribute binding
 # 2) do we have to give object data keys
+
+# eperimentation:
+# 1) differnce between the use computed properties and methods
+# 2) Investigate class and style binding more
+
+# NOTE FOR COMPONENTS
+#  Since data is now a function that returns a data object, each component will definitely have its own data
--- vueJS/main.js
+++ vueJS/main.js
@@ -1,29 +1,58 @@
-const app = new Vue ({
-    el: "#app",
+Vue.component('product',{
 
-    data: {
-        product: "LightSaber",
-        description: "May the force help you slice every Sith Head",
-        details:['10GW ion energy cell','4D emitter matrix','f/2 Flux aperture '],
-        cart: 0,
-        brand: 'Republic',
-        index: 0,
+    template:
+    `
+    <div class="product">
+            <div class="product-image">
+                <img :src="image">
+            </div>
 
-        variants: [
-            {
-                variantId :1123,
-                variantColor: 'Blue',
-                variantImage: './light_blue.png',
-                variantStock: 40,
-            },
-            {
-                variantId: 1124,
-                variantColor: 'Red',
-                variantImage: './light_red.png',
-                variantStock: 0,
-            },
-        ],
+            <div class="product-info">
+                <h1> {{ title }}</h1>
+                <p v-if="inStock" class="stock">in stock</p>
+                <p v-else class="stock" >out of stock</p>
 
+                 <ul class="colors throw">
+                    <li v-for="(variant,index) in variants" @mouseover="updateImage(index)" :key="variant.variantId" :style="{ backgroundColor: variant.variantColor }"> {{ variant.variantColor[0] }} </li>
+                </ul>
+
+                <ul>
+                    <li v-for="detail in details"> {{ detail }}</li>
+                </ul>
+                 
+                <button :disabled="!inStock" :class="{ disableButton: !inStock } " @click="addToCart()"> add to cart </button>
+
+                <div class="cart">
+                    <p> Cart ({{ cart }})</p>
+                </div> 
+            </div>
+        </div>
+    `,
+
+    data(){
+        return {
+            product: "LightSaber",
+            description: "May the force help you slice every Sith Head",
+            details:['10GW ion energy cell','4D emitter matrix','f/2 Flux aperture '],
+            cart: 0,
+            brand: 'Republic',
+            index: 0,
+
+            variants: [
+                {
+                    variantId :1123,
+                    variantColor: 'Blue',
+                    variantImage: './light_blue.png',
+                    variantStock: 40,
+                },
+                {
+                    variantId: 1124,
+                    variantColor: 'Red',
+                    variantImage: './light_red.png',
+                    variantStock: 0,
+                },
+            ],
+        }
     },
 
     methods: {
@@ -50,6 +79,18 @@ const app = new Vue ({
         inStock(){
             return this.variants[this.index].variantStock;
         }
+
+    },
+
+
+});
+
+const app = new Vue ({
+    el: "#app",
+
+    data: {
+
+        premium: false,
 
     },
 })
