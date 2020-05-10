874e6df97d1f9df9e9f404d0efcda88cd316925e
Kwaku Boateng <quacoo.boat@gmail.com>
2020-05-10 01:58:25 +0000

    axious test ---image generator

--- vueJS/test/main.js
+++ vueJS/test/main.js
@@ -1,9 +1,27 @@
- console.log(axios)
 let app = new Vue({
     el:'#app',
+    data:{
+        load: false,
+        number: 1,
+        images: [],
+    },
+    methods: {
+        generate(){
+            this.load = true;
+            axios.get(`https://jsonplaceholder.typicode.com/photos/${this.number}`)
+                 .then(
+                    (response) => {
+                        this.images = response.data;
+                        console.log(this.images);
+                        this.load = false;
+                    });
+        }
+        
+    },
 
-    mounted() {
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-             .then(response => console.log(response));
+    computed: {
+        discription(){
+            if (this.images.id) return ` ${this.images.id}: "${this.images.title}" `; else return "";
+        }
     },
 });
